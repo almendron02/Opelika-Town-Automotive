ae6f0fc09b4a2f6e92c1140e926d79876a032335
almendron <angelo.mgleza@gmail.com>
2026-03-31 07:59:15 -0500

    Update image URLs to use raw GitHub links

--- src/App.tsx
+++ src/App.tsx
@@ -192,7 +192,7 @@ export default function App() {
           >
             <div className="aspect-square rounded-xl overflow-hidden shadow-2xl transform rotate-1 sm:rotate-3 scale-100 sm:scale-105 border-4 border-white/10">
               <img 
-                src="https://github.com/almendron02/Opelika-Town-Automotive/blob/e286d07872626deae7b6f5a3cb834665e44fddb4/motor.png" 
+                src="https://raw.githubusercontent.com/almendron02/Opelika-Town-Automotive/main/motor.png" 
                 alt="High-end engine"
                 className="w-full h-full object-cover"
                 referrerPolicy="no-referrer"
@@ -379,7 +379,7 @@ export default function App() {
           <div className="flex-1 w-full">
             <div className="aspect-video bg-surface-container rounded-xl overflow-hidden shadow-2xl relative grayscale hover:grayscale-0 transition-all duration-700">
               <img 
-                src="https://github.com/almendron02/Opelika-Town-Automotive/blob/4f2620f330da761fbfa0f08e173dbae0d213d187/park.png" 
+                src="https://raw.githubusercontent.com/almendron02/Opelika-Town-Automotive/main/park.png" 
                 alt="Store Exterior"
                 className="w-full h-full object-cover"
                 referrerPolicy="no-referrer"
